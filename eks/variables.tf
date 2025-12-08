33d5f3ada15f7f9e8b507c05924539a521e3831e
tirucloud <tirucloud@gmail.com>
2025-12-08 11:47:49 +0530

    all

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,7 +1,7 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "nvg"
+  default     = "thiru"
 }
 variable "aws_vpc_name" {
   description = "The name of the VPC"
