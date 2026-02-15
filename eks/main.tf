ffe9b0b5c5c4d40aab0d5483bd3bdea3d2d6e136
tirucloud <tirucloud@gmail.com>
2026-02-15 15:34:56 +0530

    ok

--- eks/main.tf
+++ eks/main.tf
@@ -103,9 +103,9 @@ resource "aws_eks_node_group" "tiru" {
   subnet_ids      = aws_subnet.tiru_subnet[*].id
 
   scaling_config {
-    desired_size = 3
+    desired_size = 2
     max_size     = 3
-    min_size     = 3
+    min_size     = 2
   }
 
   instance_types = ["t2.medium"]
